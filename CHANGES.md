50089e95be76ce66226a3f4d9c3d4dae8ee83868
Miikka Koskinen <miikka@metosin.fi>
2018-01-24 14:38:25 +0200

    Update dependencies

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,7 @@
 ## Unreleased
 
 - Removed `maailma.encrypt` namespace
+- Updated dependencies
 
 ## 0.2.0 (2015-11-07)
 
--- project.clj
+++ project.clj
@@ -6,17 +6,17 @@
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.jasypt/jasypt "1.9.2"]
-                 [metosin/potpuri "0.2.3"]
-                 [org.clojure/tools.logging "0.3.1"]]
+                 [metosin/potpuri "0.5.1"]
+                 [org.clojure/tools.logging "0.4.0"]]
   :plugins [[lein-codox "0.10.3"]]
 
   :codox {:source-uri "http://github.com/metosin/maailma/blob/{version}/{filepath}#L{line}"}
 
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.1"]]
-                   :dependencies [[criterium "0.4.3"]
-                                  [org.clojure/clojure "1.7.0"]]
+                   :dependencies [[criterium "0.4.4"]
+                                  [org.clojure/clojure "1.9.0"]]
                    :resource-paths ["test-resources"]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-beta2"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.8"]
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.7:dev,1.8"]
             "test-clj"  ["all" "do" ["test"] ["check"]]})
